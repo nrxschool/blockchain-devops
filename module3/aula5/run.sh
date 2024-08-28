8a5709f79ed574310d193c9197f8f08590f59a76
Lucas Oliveira <olivmath@protonmail.com>
2024-08-28 20:19:43 -0300

    update: mod 3

--- module3/aula5/run.sh
+++
@@ -1,6 +0,0 @@
-# run Blockchain Network
-docker-compose -f docker-compose.nodes.yml up -d
-# run  BlockExplorer and Observability
-docker-compose -f docker-compose.blockexplorer.yml -f docker-compose.observability.yml up -d
-# create Networks and Volumes
-docker-compose -f docker-compose.networks.yml -f docker-compose.volumes.yml up -d

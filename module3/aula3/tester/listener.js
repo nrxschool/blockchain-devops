2b60d496339f5018ca05435b0d1749555237262a
Lucas Oliveira <olivmath@protonmail.com>
2024-09-10 23:21:12 -0300

    update: mod3

---
+++ module3/aula3/tester/listener.js
@@ -0,0 +1,46 @@
+const Web3 = require("web3");
+const axios = require("axios");
+
+const BESU_URL = "ws://127.0.0.1:8546";
+
+const web3 = new Web3(BESU_URL);
+const DISCORD_WEBHOOK_URL =
+  "https://discord.com/api/webhooks/1283129693154971788/oVn6BKAj88rPGGEK034csoO5ynxFZ7egIr9dmBIPXMvCeD8Nj61OSVaISFEvOJT3cDNA";
+
+const sendDiscord = async (text) => {
+  const payload = {
+    content: text,
+  };
+
+  try {
+    const response = await axios.post(DISCORD_WEBHOOK_URL, payload);
+    console.log("Mensagem enviada para o Discord:", response.status);
+  } catch (error) {
+    console.error("Erro ao enviar mensagem para o Discord:", error);
+  }
+};
+
+web3.eth.subscribe("pendingTransactions", async (error, transactionHash) => {
+  if (!error) {
+    const data = `@here\n🕒 Nova transação pendente: ${transactionHash}`;
+    console.log(data);
+    sendDiscord(data);
+  } else {
+    console.error("Erro ao ouvir transações pendentes:", error);
+  }
+});
+
+web3.eth.subscribe("newBlockHeaders", async (error, blockHeader) => {
+  if (!error) {
+    const block = await web3.eth.getBlock(blockHeader.hash, true);
+    block.transactions.forEach((transaction) => {
+      const data = `@here\n🔨 Transação minerada no bloco ${transaction.blockNumber}: , ${transaction.hash}`;
+      console.log(data);
+      sendDiscord(data);
+    });
+  } else {
+    console.error("Erro ao ouvir novos blocos:", error);
+  }
+});
+
+console.log("Ouvindo eventos e transações na mempool...");

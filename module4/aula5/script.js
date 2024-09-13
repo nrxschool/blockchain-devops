64fd0197beaf1ec6a90bbc90bd5be3d3a8d99116
Lucas Oliveira <olivmath@protonmail.com>
2024-09-13 11:06:43 -0300

    update aula5

--- module4/aula5/script.js
+++ module4/aula5/script.js
@@ -1,50 +1,49 @@
 import eth from "k6/x/ethereum";
-import { sleep } from "k6";
+import http from "k6/http";
+import { check, sleep } from "k6";
+import { Counter } from "k6/metrics";
 
-const privateKey =
-  "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-const from = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
-const to = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
-const amount = Number(0.1 * 1e18);
-const rpcUrl = "http://localhost:8545";
+const PRIVATE_KEY = "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+const BOB = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
+const NODE_RPC_URL = "http://127.0.0.1:8545";
+let NONCE = 0
 
-const client = new eth.Client({
-  url: rpcUrl,
-  privateKey: privateKey,
-});
-
-export const options = {
+export let options = {
   stages: [
-    { duration: "10s", target: 20 },
-    { duration: "10s", target: 10 },
-    { duration: "10s", target: 0 },
+    // Aumenta gradualmente o número de usuários virtuais
+    { duration: "30s", target: 1}, // 30s com 1 usuários
+    { duration: "30s", target: 5}, // 30s com 5 usuários
+    { duration: "30s", target: 10}, // 30s com 10 usuários
+    { duration: "30s", target: 50}, // 30s com 50 usuários
+    { duration: "30s", target: 100}, // 30s com 100 usuários
+    { duration: "30s", target: 200}, // 30s com 200 usuários
+    { duration: "30s", target: 0 }, // Finaliza removendo todos os usuários
   ],
 };
 
-export function setup() {
-  return { nonce: client.getNonce(from) };
-}
+const txSended = new Counter("txSended");
 
-function sendTx(data) {
-  console.log(`nonce => ${data.nonce}`);
-  const gas = client.gasPrice();
-  console.log(`gas price => ${gas}`);
+export default function () {
+  const client = new eth.Client({
+    url: NODE_RPC_URL,
+    privateKey: PRIVATE_KEY,
+  });
 
-  const bal = client.getBalance(from, client.blockNumber());
-  console.log(`bal => ${bal}`);
+  console.log(`NONCE >>> ${NONCE}`);
+  const GAS = client.gasPrice();
+  console.log(`GAS >>> ${GAS}`);
 
   const tx = {
-    to: to,
-    value: amount,
-    gas_price: gas,
-    nonce: data.nonce,
+    to: BOB,
+    value: Number(0.0001 * 1e18),
+    gas_price: GAS,
+    nonce: NONCE,
   };
 
-  const txh = client.sendRawTransaction(tx);
-  console.log("tx hash => " + txh);
-  data.nonce = data.nonce + 1;
-}
-export default function (data) {
-  sendTx(data);
-  sleep(1);
+  const TX_HASH = client.sendRawTransaction(tx);
+  console.log(`TX_HASH >>> ${TX_HASH}`);
+
+  NONCE++;
+
+  txSended.add(1);
 }

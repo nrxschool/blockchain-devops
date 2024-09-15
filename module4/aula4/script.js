056d3e109d5faaded75a1204090438e214a44a7e
Lucas Oliveira <olivmath@protonmail.com>
2024-09-15 19:06:17 -0300

    remove aula 4

--- module4/aula4/script.js
+++ module4/aula4/script.js
@@ -1,25 +1,61 @@
-import http from "k6/http";
-import { check, sleep } from "k6";
+import eth from "k6/x/ethereum";
+import { Counter, Gauge, Trend } from "k6/metrics";
+
+const PRIVATE_KEY =
+  "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+const ALICE = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+const BOB = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
+const NODE_RPC_URL = "http://127.0.0.1:8545";
+let NONCE = 0;
 
 export let options = {
   stages: [
     // Aumenta gradualmente o número de usuários virtuais
-    { duration: "45s", target: 100 }, // 1 minuto com 100 usuários
-    { duration: "45s", target: 500 }, // 1 minuto com 500 usuários
-    { duration: "45s", target: 1000 }, // 1 minuto com 1000 usuários
-    { duration: "45s", target: 5000 }, // 1 minuto com 5000 usuários
-    { duration: "45s", target: 10000 }, // 1 minuto com 10000 usuários
-    { duration: "45s", target: 20000 }, // 1 minuto com 20000 usuários
-    { duration: "45s", target: 0 }, // Finaliza removendo todos os usuários
+    { duration: "30s", target: 1 }, // 30s com 1 usuários
+    { duration: "30s", target: 5 }, // 30s com 5 usuários
+    { duration: "30s", target: 10 }, // 30s com 10 usuários
+    { duration: "30s", target: 50 }, // 30s com 50 usuários
+    { duration: "30s", target: 100 }, // 30s com 100 usuários
+    { duration: "30s", target: 200 }, // 30s com 200 usuários
+    { duration: "30s", target: 0 }, // Finaliza removendo todos os usuários
   ],
 };
 
-export default function () {
-  let res = http.get("http://nginx");
+const nonceCounter = new Counter("Nonce");
+const ethSended = new Counter("EthSended");
+const gasUsedGauge = new Gauge("GasUsedGauge");
+const txMinedTime = new Trend("TxMinedTime");
 
-  const checkResult = check(res, {
-    "status was 200": (r) => r.status == 200,
+export default function () {
+  const client = new eth.Client({
+    url: NODE_RPC_URL,
+    privateKey: PRIVATE_KEY,
   });
 
-  sleep(1);
+  const GAS = client.gasPrice();
+
+  let prev_nonce = client.getNonce(ALICE);
+  if (NONCE < prev_nonce) {
+    NONCE = prev_nonce;
+  }
+
+  const tx = {
+    to: BOB,
+    value: Number(0.0001 * 1e18),
+    gas_price: GAS,
+    nonce: NONCE,
+  };
+
+  const startTime = new Date().getTime();
+  const TX_HASH = client.sendRawTransaction(tx);
+  client.waitForTransactionReceipt(TX_HASH).then((txMined) => {
+    const endTime = new Date().getTime();
+
+    ethSended.add(Number(0.0001 * 1e18));
+    gasUsedGauge.add(txMined.gas_used);
+    txMinedTime.add(endTime - startTime);
+  });
+
+  NONCE++;
+  nonceCounter.add(1);
 }
--- module4/aula5/roteiro.md
+++
@@ -1,7 +0,0 @@
-**Instalar xk6**
-
-```bash
-docker run --rm -e GOOS=darwin -u "$(id -u):$(id -g)" -v "${PWD}:/xk6" \
-  grafana/xk6 build \
-  --with github.com/distribworks/xk6-ethereum
-```
